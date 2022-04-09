a39cb914d940c1e3e94c2a6bc71f81cf72e8098c
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-04-08 19:18:10 -0700

    better preview thingo

--- WebItemEditor/webroot/js/webedititem.js
+++ WebItemEditor/webroot/js/webedititem.js
@@ -13,11 +13,16 @@ function loaditemdata() {
             enchantmentDropdown.insertAdjacentHTML("beforeend", "<option>" + obj.available_ench_types[enchant_type] + "</option>");
         }
         document.getElementById('hide_data').checked = obj.hides == "true";
+        renderPreview();
     });
     document.getElementById('add_ench_button').addEventListener("click", function() {
         let type = document.getElementById('add_enchantment_type').value;
         let level = document.getElementById('add_enchant_level').value;
         listEnchant(type, level);
+        renderPreview();
+    });
+    document.getElementById('hide_data').addEventListener("click", function() {
+        renderPreview();
     });
     document.getElementById('sendback_button').addEventListener("click", function() {
         let object = {
@@ -42,5 +47,71 @@ function listEnchant(enchant, level) {
     document.getElementById("remove_ench_" + enchant).addEventListener("click", function() {
         document.getElementById("ench_list_" + enchant).remove();
         delete enchantsKnown[enchant];
+        renderPreview();
     });
+}
+
+function appendText(previewObj, text) {
+    let color = "#ffffff";
+    let bold = false, italic = false, underline = false, strike = false;
+    for (var i = 0; i < text.length; i++) {
+        let char = text.charAt(i);
+        if (char == "\n") {
+            previewObj.appendChild(document.createElement('br'));
+        }
+        else if (char == '&' && i + 1 < text.length) {
+            i++;
+            switch (text.charAt(i)) {
+                case '0': color = "#000000"; break;
+                case '1': color = "#0000AA"; break;
+                case '2': color = "#00AA00"; break;
+                case '3': color = "#00AAAA"; break;
+                case '4': color = "#AA0000"; break;
+                case '5': color = "#AA00AA"; break;
+                case '6': color = "#FFAA00"; break;
+                case '7': color = "#AAAAAA"; break;
+                case '8': color = "#555555"; break;
+                case '9': color = "#5555FF"; break;
+                case 'a': case 'A': color = "#55FF55"; break;
+                case 'b': case 'B': color = "#55FFFF"; break;
+                case 'c': case 'C': color = "#FF5555"; break;
+                case 'd': case 'D': color = "#FF55FF"; break;
+                case 'e': case 'E': color = "#FFFF55"; break;
+                case 'f': case 'F': color = "#ffffff"; break;
+                case 'r': case 'R': color = "#ffffff"; bold = false; strike = false; underline = false; italic = false; break;
+                case 'k': case 'K': break;
+                case 'l': case 'L': bold = true; break;
+                case 'm': case 'M': strike = true; break;
+                case 'n': case 'N': underline = true; break;
+                case 'o': case 'O': italic = true; break;
+                case '#': if (i + 7 < text.length) { color = text.substring(i, i + 7); i += 6; } break;
+            }
+        }
+        else {
+            let newSpan = document.createElement('span');
+            newSpan.style.color = color;
+            newSpan.style.fontWeight = bold ? "bold" : "normal";
+            newSpan.style.fontStyle = italic ? "italic" : "normal";
+            newSpan.style.textDecoration = strike ? (underline ? "underline line-through" : "line-through") : (underline ? "underline" : "none");
+            newSpan.textContent = char;
+            previewObj.appendChild(newSpan);
+        }
+    }
+}
+
+function renderPreview() {
+    let previewObj = document.getElementById('item_preview');
+    while (previewObj.firstChild) {
+        previewObj.removeChild(previewObj.firstChild);
+    }
+    let hideExtras = document.getElementById('hide_data').checked;
+    let display = document.getElementById('display_name').value;
+    let lore = document.getElementById('lore').value;
+    appendText(previewObj, display + "\n");
+    if (!hideExtras) {
+        for (var enchant_type in enchantsKnown) {
+            appendText(previewObj, "&7" + enchant_type + " " + enchantsKnown[enchant_type] + "\n");
+        }
+    }
+    appendText(previewObj, lore);
 }
